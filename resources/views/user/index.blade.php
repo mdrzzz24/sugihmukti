08eac3fb9fac8ba6158ff6f4f3e60b6ad0998a5d
farahhelmi <farahhelmi91@gmail.com>
2024-08-16 13:50:37 +0700

    update : icon newspaper

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -96,7 +96,10 @@
     <section id="tentang-berita" class= "bg-light">
         <div class="container py-5 w-75">
             <div class="d-flex align-items-center mb-4">
-                <img src="{{ asset('images/newspaper.png') }}" alt="Berita" class="img-fluid rounded mb-0 me-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-newspaper me-3" viewBox="0 0 16 16">
+                  <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z"/>
+                  <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z"/>
+                </svg>
                 <h1 class="mb-0">Berita Desa</h1>
             </div>
 
